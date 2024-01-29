afd5a5f1f0384129b45d5dacb967a72f702115af
Saeed Norouzi <saeednorouzi98@gmail.com>
2024-01-29 21:14:47 +0330

    Feat(first route): create and implant first router
    Description:
    In order to increase the level of understanding of the routing system
    in fastify, I have added some simple routes to the project which are
    responsible for retrieving some sample data and send them to client.
    
    Change Logs:
    [Sample data] [Added] - Add sample data to be used in client response.
    [Main config] [Modified] - Add simple routes.

---
+++ products.js
@@ -0,0 +1,18 @@
+export const products = [
+	{
+		id: 1,
+		name: "Notebook",
+	},
+	{
+		id: 2,
+		name: "Pen",
+	},
+	{
+		id: 3,
+		name: "Pencil",
+	},
+	{
+		id: 4,
+		name: "Eraser",
+	},
+];
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 /** import fastify module */
 import Fastify from "fastify";
+/** import products list */
+import { products } from "./products.js";
 
 /** create an instants from fastify */
 const fastify = Fastify({
@@ -8,6 +10,38 @@ const fastify = Fastify({
 
 /** define application PORT number */
 const PORT = 3000;
+
+/**
+ * create a simple route with fastify
+ */
+fastify.get("/", (req, reply) => {
+	reply.send({
+		message: "Hello from fastify",
+	});
+});
+
+/**
+ * create a route to retrieve a list of products
+ */
+fastify.get("/products", (req, reply) => {
+	reply.send(products);
+});
+
+/**
+ * create a route to retrieve single product
+ */
+fastify.get("/products/single/:id", (req, reply) => {
+	/** extract id from request */
+	const { id } = req.params;
+	/** find the requested product from product list */
+	const product = products.find((p) => p.id === +id);
+	/** send error if the product was not found */
+	if (!product) {
+		return reply.code(404).send({ message: "Product was notfound" });
+	}
+	/** send success response */
+	return reply.send(product);
+});
 
 const main = async () => {
 	try {
