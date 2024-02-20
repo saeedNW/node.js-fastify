37c10b3c9455c72462ae93f8ce677934d91e47ec
Saeed Norouzi <saeednorouzi98@gmail.com>
2024-02-21 00:18:56 +0330

    Refactor(models): refactor user data model
    Description:
    In this commit, I have added a set of new data in user model,
    which will be saved as a separated table and has a connection
    to users table.
    
    Change Logs:
    [User model] [Modiried] - Add new data model to user's data mdoel.

--- models/user.model.js
+++ models/user.model.js
@@ -3,27 +3,35 @@ import { Model, DataTypes } from "sequelize";
 /** import sequelize connection config */
 import { sequelize } from "../configs/sequelize.config.js";
 
-/**
- * represents a User in the database.
- * @class
- * @extends Model
- */
-export class User extends Model {}
-
 /** initialize the User model with sequelize */
-User.init(
-	{
-		id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-		firstName: { type: DataTypes.STRING },
-		lastName: { type: DataTypes.STRING },
-		accessToken: { type: DataTypes.STRING, defaultValue: "" },
-		username: { type: DataTypes.STRING, allowNull: false, unique: true },
-		password: { type: DataTypes.STRING, allowNull: false },
-		active: { type: DataTypes.BOOLEAN, defaultValue: false },
-		birthday: { type: DataTypes.DATE },
-	},
-	/** Configuration options */
-	{ sequelize, name: "users" }
-);
+export const User = sequelize.define("User", {
+	id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+	firstName: { type: DataTypes.STRING },
+	lastName: { type: DataTypes.STRING },
+	accessToken: { type: DataTypes.STRING, defaultValue: "" },
+	username: { type: DataTypes.STRING, allowNull: false, unique: true },
+	password: { type: DataTypes.STRING, allowNull: false },
+	active: { type: DataTypes.BOOLEAN, defaultValue: false },
+	birthday: { type: DataTypes.DATE },
+});
 
-// User.sync({force:true})
+/** initialize the User's details model with sequelize */
+export const UserDetail = sequelize.define("UserDetail", {
+	id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+	address: { type: DataTypes.STRING },
+	latitudes: { type: DataTypes.DOUBLE },
+	longitudes: { type: DataTypes.DOUBLE },
+	UserId: { type: DataTypes.INTEGER },
+});
+
+/** define the connection between user and user details models */
+User.hasOne(UserDetail);
+UserDetail.belongsTo(User);
+
+// User.sync({ alter: true }).then(() => {
+// 	console.log("User Sync completed");
+// });
+
+// UserDetail.sync({ alter: true }).then(() => {
+// 	console.log("UserDetail Sync completed");
+// });
